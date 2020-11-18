d4ea1351b573051fdb1fc58e5303951fdc0f95f8
unknown <iamwattanas@gmail.com>
2020-11-18 20:58:57 +0700

    mix thing up

--- src/auth.ts
+++ src/auth.ts
@@ -2,15 +2,11 @@ import { sign } from "jsonwebtoken";
 import { REFRESH_TOKEN_SECRET, ACCESS_TOKEN_SECRET } from "./constants";
 
 export const createTokens = (user: any) => {
-  const refreshToken = sign(
-    { userId: user.id },
-    REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: "7d"
-    }
-  );
+  const refreshToken = sign({ userId: user.id }, REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
   const accessToken = sign({ userId: user.id }, ACCESS_TOKEN_SECRET, {
-    expiresIn: "15s"
+    expiresIn: "15s",
   });
 
   return { refreshToken, accessToken };
--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,24 @@ import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import resolvers from "./resolvers";
 import typeDefs from "./typeDefs";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } from "./constants";
-import { verify } from "jsonwebtoken"
+import { sign, verify } from "jsonwebtoken";
 import { createTokens } from "./auth";
 
 const APP_PORT = process.env.PORT || 4000;
 
-const server = new ApolloServer({ 
-  typeDefs, 
-  resolvers, 
-  context: ({ req, res }: any) => ({ req, res })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req, res }: any) => ({ req, res }),
 });
 
 const app = express();
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(async (req: any, _, next) => {
+app.use(async (req: any, res: any, next) => {
   const refreshToken = req.cookies["x-refresh-token"] || null;
   const accessToken = req.cookies["x-access-token"] || null;
 
@@ -31,9 +31,10 @@ app.use(async (req: any, _, next) => {
     try {
       const data: any = verify(accessToken, ACCESS_TOKEN_SECRET);
       req.userId = data.userId;
+      return next();
     } catch {}
   }
-  
+
   if (!refreshToken) {
     return next();
   }
@@ -46,11 +47,28 @@ app.use(async (req: any, _, next) => {
     return next();
   }
 
-  // const tokens = createTokens(user);
+  console.log(data);
 
-  // res.cookie("refresh-token", tokens.refreshToken);
-  // res.cookie("access-token", tokens.accessToken);
-  // req.userId = user.id;
+  // const user = await User.findOne(data.userId);
+  // // token has been invalidated
+  // if (!user || user.count !== data.count) {
+  //   return next();
+  // }
+
+  const _refreshToken: any = sign(
+    { userId: data.userId },
+    REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+  const _accessToken = sign({ userId: data.userId }, ACCESS_TOKEN_SECRET, {
+    expiresIn: "15min",
+  });
+
+  res.cookie("x-refresh-token", _refreshToken, { httpOnly: true });
+  res.cookie("x-access-token", _accessToken, { httpOnly: true });
+  req.userId = data.userId;
 
   next();
 });
@@ -58,5 +76,7 @@ app.use(async (req: any, _, next) => {
 server.applyMiddleware({ app });
 
 app.listen(APP_PORT, () => {
-  console.log(`Server is ready at http://localhost:${APP_PORT}${server.graphqlPath}`)
-})
+  console.log(
+    `Server is ready at http://localhost:${APP_PORT}${server.graphqlPath}`
+  );
+});
